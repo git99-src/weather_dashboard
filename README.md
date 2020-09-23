e0e8362835f0f8b67a029051e3600f914ad8ab4a
git99.src <cpu.al2@gmail.com>
2020-09-22 23:40:52 -0700

    Final check and commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # weather_dashboard
 The application will allow a user to check the 5 day weather forecast for a city - useful for planning a trip.
+
+https://git99-src.github.io/weather_dashboard/
+
+## Description
+
+This calendar application will be useful for those who wish to plan a trip.  The user will be presented with a search prompt which they can use to enter a city that they wish to check the weather.  The current weather information will then be presented as well as the 5 day forecast. The weather information includes - temperature, wind speed, humidity and UV index. 
+
+
+## Installation
+
+This application is hosted in the Github repository.  It was written using momentjs, javascript, HTML, CSS, JQuery and Bootstrap v4.  The openweather api was used extensively to gather the weather data presented on this site.
+
+## Author
+
+Al Pineda
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,204 +1,209 @@
-
-var apiKey= "a4d8a766435d39d19416a58ba78c0da0";    
-cities = [
-        "San Diego",
-        "Denver",
-        "Miami",    
-];  
-var city = " ";     
+var savID = 0;
+var lastCity = " ";
+var apiKey = "a4d8a766435d39d19416a58ba78c0da0";
+cities = [""];
+var city = " ";
 
 function getCityInfo(city) {
 
-    
+
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  // construct a url to search OpenWeatherAPI for the current weather in the
+  // city
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  // send ajax request for current weather to OpenWeatherAPI
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (data) {
+
+
+
+    // today's date
+    var today = moment(new Date()).format("L");
+
+    // build current weather card
+    $("#city-name").text(data.name + " " + today)
+    // display the temperature, wind, and humidity 
+    var tempDiv = $("<p>").attr("class", "card-text").text("Temperature: " + data.main.temp + " °F");
+    var humidityDiv = $("<p>").attr("class", "card-text").text("Humidity: " + data.main.humidity + "%");
+    var windDiv = $("<p>").attr("class", "card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+
+    // create url for weather icon and save it in a variable
+    var iconUrl =
+      "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+
+    // create an img element and set its src equal to the icon url.
+    var iconImg = $("<img>").attr({
+      src: iconUrl,
+      alt: data.weather[0].description,
+    });
+    // empty div #weather-icon and append the current weather
+    //    $("#weather-icon").empty().append(iconImg,tempDiv,humidityDiv,windDiv);
+
+
+    // construct a url to search OpenWeatherAPI for UV index
     var queryUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=imperial&appid=" +
-      apiKey;
-   
-    // construct a url to search OpenWeatherAPI for the current weather in the
-    // city
-    var queryUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=imperial&appid=" +
-      apiKey;
-
-    // send ajax request for current weather to OpenWeatherAPI
-    $.ajax({
-      url: queryUrl,
-      method: "GET",
-    }).then(function (data) {
-      // log the data from the api to the console
-      console.log(data);
-      // date
-
-      // set the text of the #city-name h2 element using the city name in the
-      // response
-      $("#city-name").text(data.name + " Weather");
-      
-        // display the temperature, wind, and humidity 
-        var tempDiv = $("<p>").attr("class", "card-text").text("Temperature: " + data.main.temp + " °F");
-         
-        var humidityDiv = $("<p>").attr("class", "card-text").text("Humidity: "+ data.main.humidity + "%");
-    
-        var windDiv = $("<p>").attr("class", "card-text").text("Wind Speed: " +data.wind.speed + " MPH");
-       
-        
-      // create url for weather icon and save it in a variable
-      var iconUrl =
-        "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-
-      // create an img element and set its src equal to the icon url.
-      var iconImg = $("<img>").attr({
-        src: iconUrl,
-        alt: data.weather[0].description,
-      });
-      // empty div #weather-icon and append the current weather
-//    $("#weather-icon").empty().append(iconImg,tempDiv,humidityDiv,windDiv);
- 
-   
-  // construct a url to search OpenWeatherAPI for UV index
-      var queryUrl =
       "http://api.openweathermap.org/data/2.5/uvi?lat=" +
       data.coord.lat +
       "&lon=" +
       data.coord.lon +
       "&appid=" +
       apiKey;
-      
-    
+
+
     // send ajax request for UV index to OpenWeatherAPI
     $.ajax({
       url: queryUrl,
       method: "GET",
     }).then(function (data2) {
       // log the data from the api to the console
-      console.log(data2);
-      
-     if (parseInt(data2.value) < 3 ){
-      uvDiv = $("<p>").attr("class", "card-text text-success").text("UV Index: " + data2.value); 
-      } else if (parseInt(data2.value) >= 6 ){
-      uvDiv = $("<p>").attr("class", "card-text text-danger").text("UV Index: " + data2.value); 
+
+      if (parseInt(data2.value) < 3) {
+        uvDiv = $("<p>").attr("class", "card-text text-success").text("UV Index: " + data2.value);
+      } else if (parseInt(data2.value) >= 6) {
+        uvDiv = $("<p>").attr("class", "card-text text-danger").text("UV Index: " + data2.value);
       } else {
-      uvDiv = $("<p>").attr("class", "card-text text-warning").text("UV Index: " + data2.value); 
+        uvDiv = $("<p>").attr("class", "card-text text-warning").text("UV Index: " + data2.value);
       };
       // date
-      $("#weather-icon").empty().append(iconImg,tempDiv,humidityDiv,windDiv,uvDiv);
-      // set the text of the #city-name h2 element using the city name in the
-      // response
-    //   $("#city-name").text(data.index + " Weather");
-    getFiveDay(city);
-      
+      $("#weather-icon").empty().append(iconImg, tempDiv, humidityDiv, windDiv, uvDiv);
+       getFiveDay(city);
+
     });
-      
-    });
+  });
 };
 
-
-function getFiveDay(city){
+function getFiveDay(city) {
   var queryUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
     "&units=imperial&appid=" +
     apiKey;
+
   // send ajax request for UV index to OpenWeatherAPI
   $.ajax({
-  url: queryUrl,
-  method: "GET",
+    url: queryUrl,
+    method: "GET",
   }).then(function (data3) {
-  // log the data from the api to the console
-  console.log(data3);
 
-  // grab a reference to the card container
-  var cardContainer = $("#card-5day");
+    // grab a reference to the card container
+    var cardContainer = $("#card-5day");
 
-  cardContainer.empty();
+    var h3Div = $("<h3>5 Day Forecast</h3>");
+    $(".five-day").append(h3Div);
 
-  for (i = 0; i < 35; i+=7) {
+    cardContainer.empty();
 
+    // loop to get 5 day data
+    for (i = 0; i < 40; i += 8) {
+      var iconUrl =
+        "https://openweathermap.org/img/wn/" + data3.list[i].weather[0].icon + ".png";
 
-    var cardTitle = $("<h5>").text(data3.list[i].dt).addClass("card-title");
-    var cardBody = $("<div class='card bg-primary text-light'></div>");
-    cardBody.append(cardTitle);
-    var temp5Div = $("<p>").attr("class", "card-text").text("Temp: " + data3.list[i].main.temp + " °F");
-    var humidity5Div = $("<p>").attr("class", "card-text").text("Humidity: " + data3.list[i].main.humidity + "%");
-    var card = $("<div class='card bg-primary text-light'></div>");
-    card.append(cardBody, temp5Div, humidity5Div);
-    var col = $("<div class='col'></div>");
-    col.append(card);
-    cardContainer.append(col);
+      var iconImg = $("<img>").attr({
+        src: iconUrl,
+        alt: data3.list[i].weather[0].description,
+      });
 
+      //format date 2020-09-22 03:00:00
+      var day = data3.list[i].dt_txt.split("-")[2].split(" ")[0];
+      var month = data3.list[i].dt_txt.split("-")[1];
+      var year = data3.list[i].dt_txt.split("-")[0];
+      var dateFmt = month + "/" + day + "/" + year
 
+      // build 5 day cards 
+      var cardTitle = $("<h5>").text(dateFmt).addClass("card-title");
+      var cardBody = $("<div class='card bg-primary text-light'></div>");
+      cardBody.append(cardTitle);
+      var temp5Div = $("<p>").attr("class", "card-text").text("Temp: " + data3.list[i].main.temp + " °F");
+      var humidity5Div = $("<p>").attr("class", "card-text").text("Humidity: " + data3.list[i].main.humidity + "%");
+      var card = $("<div class='card bg-primary text-light'></div>");
+      card.append(cardBody, iconImg, temp5Div, humidity5Div);
+      var col = $("<div class='col'></div>");
+      col.append(card);
+      cardContainer.append(col);
+
+    };
+  });
+};
+
+function loadCity() {
+
+  $("#city-view").empty();
+  cities = [""];
+
+  // get cities in local storage and add to array
+  for (idCity = 0; idCity < 20; idCity++) {
+
+    //If I have a city in local storage
+    if (localStorage.getItem(idCity) !== null) {
+    
+      cities.push(localStorage.getItem(idCity));
+      savID = idCity
+
+    };
   };
 
-  });
-  
+  // Delete array of cities prior to adding new city
+  $("#city-view").empty();
+  $("#city-selected").empty();
+  // Loops through the array of cities
+  for (var i = 0; i < cities.length; i++) {
 
+    // render city history buttons dynamically
+    var a = $("<button>");
+    a.attr("data-name", cities[i]);
+    a.attr("id", "city-selected");
+    a.text(cities[i]);
+    $("#city-view").append(a);
+
+    // save last city for when page is reloaded
+    lastCity = cities[i];
+
+  }
 };
 
-function loadCity(){
-        
-        // // get cities in local storage and add to array
-        // for (j = 0; j < 5; j++) {
-        
-        //     var id = "city-" + j;
-        //     var appointment = localStorage.getItem(id);
+// if search button is clicked - valid then go
+$(document).on("click", ".city-search", function (event) {
+  event.preventDefault();
 
-        //     // If I have a city in local storage
-        //     if (appointment !== null) {
-        //          $(this).children(".description").val(appointment);
-        //     };
+  // This line of code will grab the input from search
+  var city = $("#city-input").val().trim();
 
-        // };
-        
-        // Delete array of cities prior to adding new city
-        $("#city-view").empty();
+  if (city != "") {
 
-        // Loops through the array of cities
-        for (var i = 0; i < cities.length; i++) {
-          // render city history buttons dynamically
-          var a = $("<button>");
-          // Adds a class of movie to our button
-        //   a.addClass("city");
-          // Added a data-attribute
-          a.attr("data-name", cities[i]);
-          a.attr("id", "city-selected");
-          // Provided the initial button text
-          a.text(cities[i]);
-          // Added the button to the buttons-view div
-          $("#city-view").append(a);
-        }
-};
-
-    // if search button is clicked - valid then go
-    $(document).on("click", ".city-search", function(event) {
-        event.preventDefault();
-        // This line of code will grab the input from the textbox
-        var city = $("#city-input").val().trim();
-
-        if (city != "") {
-        
-            // get the JSON data from weather API
-            getCityInfo(city); 
-            // Add city to Local storage (array)
-            cities.push(city);
-            // load city history
-            loadCity();
-       
-
-        };
-    });
-
-    // if city is clicked 
-    $(document).on("click", "#city-selected", function() {
-     // if search button is clicked - valid then go
-        var city = $(this).text().trim();
-        
-        alert(city)
-        getCityInfo(city);
-        
-        
-    });
-
+    // get the JSON data from weather API
+    getCityInfo(city);
+    // load city history
+    var savThis = 0;
+    // increment to save to local storage
+    savThis = savID + 1
+    // add to local storage
+    localStorage.setItem(savThis, city);
+    // load city history
     loadCity();
+
+  };
+});
+
+// if city is clicked 
+$(document).on("click", "#city-selected", function () {
+  // if search button is clicked - valid then go
+  var city = $(this).text().trim();
+
+  getCityInfo(city);
+
+});
+
+loadCity();
+getCityInfo(lastCity);
